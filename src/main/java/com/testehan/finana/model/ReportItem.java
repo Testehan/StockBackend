1ee14a2f4f442a5b25168462d998b53fd3e15a56
DanTestehan <tdan89@yahoo.com>
2025-10-20 20:20:20 +0100

    finana -  added personalNotes field so that i can input various remarks for items if i want

--- src/main/java/com/testehan/finana/model/ReportItem.java
+++ src/main/java/com/testehan/finana/model/ReportItem.java
@@ -11,4 +11,11 @@ public class ReportItem {
     private String name;
     private Integer score;
     private String explanation;
+    private String personalNotes;
+
+    public ReportItem(String name, Integer score, String explanation) {
+        this.name = name;
+        this.score = score;
+        this.explanation = explanation;
+    }
 }
