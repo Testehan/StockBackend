5d750d04faae0da15204b79f45a7e043b202832a
DanTestehan <tdan89@yahoo.com>
2025-11-03 20:20:20 +0100

    finana - removed some stock statuses

--- src/main/java/com/testehan/finana/model/UserStockStatus.java
+++ src/main/java/com/testehan/finana/model/UserStockStatus.java
@@ -1,13 +1,11 @@
 package com.testehan.finana.model;
 
 public enum UserStockStatus {
-    NEW,
-    RESEARCHING,
-    WATCHLIST,
-    BUY_CANDIDATE,
+    NEW,                        // just added, no work done
+    RESEARCHING,                // working on this
+    WATCHLIST,                  // good companies but not sure yet if good enough to be moved to buy candidates
+    BUY_CANDIDATE,              // awesome companies where i wait for better price
     OWNED,
-    TRIM,
-    SELL,
-    PASS,
-    BLACKLIST
+    PASS                      // i don't like something about this ..could be domain...risks..etc
+
 }
